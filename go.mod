e151bee2c517ac80af3c37cc0bb17d0e3665ee72
Lydia Gorham <lydia@stytch.com>
2024-06-05 12:49:45 -0700

    Add Sessions

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/sessions v1.2.2
 	github.com/joho/godotenv v1.5.1
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/stytchauth/stytch-go/v11 v11.5.2
+	github.com/stytchauth/stytch-go/v12 v12.5.1
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- main.go
+++ main.go
@@ -10,16 +10,22 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	gorillaSessions "github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
-	"github.com/stytchauth/stytch-go/v11/stytch/consumer/magiclinks"
-	"github.com/stytchauth/stytch-go/v11/stytch/consumer/magiclinks/email"
-	"github.com/stytchauth/stytch-go/v11/stytch/consumer/stytchapi"
+	"github.com/stytchauth/stytch-go/v12/stytch/consumer/magiclinks"
+	"github.com/stytchauth/stytch-go/v12/stytch/consumer/magiclinks/email"
+	"github.com/stytchauth/stytch-go/v12/stytch/consumer/sessions"
+	"github.com/stytchauth/stytch-go/v12/stytch/consumer/stytchapi"
+	"github.com/stytchauth/stytch-go/v12/stytch/consumer/users"
+)
+
+var (
+	store = gorillaSessions.NewCookieStore([]byte("your-secret-key"))
 )
 
 type config struct {
 	address      string
 	fullAddress  string
-	magicLinkURL string
 	stytchClient *stytchapi.API
 }
 
@@ -27,6 +33,7 @@ type config struct {
 type templateVariables struct {
 	LoginOrCreateUserPath string
 	LoggedOutPath         string
+	EmailAddress          string
 }
 
 func main() {
@@ -56,11 +63,22 @@ func main() {
 
 // handles the homepage for Hello Socks
 func (c *config) homepage(w http.ResponseWriter, r *http.Request) {
+	user := c.getAuthenticatedUser(w, r)
+
+	if user != nil {
+		parseAndExecuteTemplate(
+			"templates/loggedIn.html",
+			&templateVariables{LoggedOutPath: c.fullAddress + "/logout", EmailAddress: user.Emails[0].Email},
+			w,
+		)
+	}
+
 	parseAndExecuteTemplate(
 		"templates/loginOrSignUp.html",
 		&templateVariables{LoginOrCreateUserPath: c.fullAddress + "/login_or_create_user"},
 		w,
 	)
+
 }
 
 // takes the email entered on the homepage and hits the stytch
@@ -69,9 +87,7 @@ func (c *config) loginOrCreateUser(w http.ResponseWriter, r *http.Request) {
 	_, err := c.stytchClient.MagicLinks.Email.LoginOrCreate(
 		context.Background(),
 		&email.LoginOrCreateParams{
-			Email:              r.FormValue("email"),
-			LoginMagicLinkURL:  c.magicLinkURL,
-			SignupMagicLinkURL: c.magicLinkURL,
+			Email: r.FormValue("email"),
 		})
 	if err != nil {
 		log.Printf("something went wrong sending magic link: %s\n", err)
@@ -83,25 +99,66 @@ func (c *config) loginOrCreateUser(w http.ResponseWriter, r *http.Request) {
 // this is the endpoint the link in the magic link hits takes the token from the
 // link's query params and hits the stytch authenticate endpoint to verify the token is valid
 func (c *config) authenticate(w http.ResponseWriter, r *http.Request) {
-	_, err := c.stytchClient.MagicLinks.Authenticate(
+	resp, err := c.stytchClient.MagicLinks.Authenticate(
 		context.Background(),
 		&magiclinks.AuthenticateParams{
-			Token: r.URL.Query().Get("token"),
+			Token:                  r.URL.Query().Get("token"),
+			SessionDurationMinutes: 60,
 		})
 	if err != nil {
 		log.Printf("something went wrong authenticating the magic link: %s\n", err)
 	}
 
-	parseAndExecuteTemplate(
-		"templates/loggedIn.html",
-		&templateVariables{LoggedOutPath: c.fullAddress + "/logout"},
-		w,
-	)
+	session, err := store.Get(r, "stytch_session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["token"] = resp.SessionToken
+	session.Save(r, w)
+
+	c.homepage(w, r)
 }
 
 // handles the logout endpoint
 func (c *config) logout(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "stytch_session")
+	if err != nil {
+		log.Printf("error getting gorilla session: %s\n", err)
+	}
+	session.Options.MaxAge = -1
+	session.Save(r, w)
+
 	parseAndExecuteTemplate("templates/loggedOut.html", nil, w)
+}
+
+// handles returning the authenticated user, if valid session present
+func (c *config) getAuthenticatedUser(w http.ResponseWriter, r *http.Request) *users.User {
+	session, err := store.Get(r, "stytch_session")
+	if err != nil || session == nil {
+		return nil
+	}
+
+	token, ok := session.Values["token"].(string)
+	if !ok || token == "" {
+		return nil
+	}
+
+	resp, err := c.stytchClient.Sessions.Authenticate(
+		context.Background(),
+		&sessions.AuthenticateParams{
+			SessionToken: token,
+		})
+	if err != nil {
+		delete(session.Values, "token")
+		session.Save(r, w)
+		return nil
+	}
+	session.Values["token"] = resp.SessionToken
+	session.Save(r, w)
+
+	return &resp.User
 }
 
 // helper function to parse the template & render it with any provided data
@@ -148,7 +205,6 @@ func initializeConfig() (*config, error) {
 	return &config{
 		address:      address,
 		fullAddress:  "http://" + address,
-		magicLinkURL: "http://" + address + "/authenticate",
 		stytchClient: stytchAPIClient,
 	}, nil
 
